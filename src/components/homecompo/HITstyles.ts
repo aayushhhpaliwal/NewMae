358aa10f7c3a10b056f5cd35bb772027cf2ecbc9
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2023-10-03 14:23:54 +0530

    styled component

---
+++ src/components/homecompo/HITstyles.ts
@@ -0,0 +1,53 @@
+// styles.ts
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  height: 100vh;
+`;
+
+export const TestSection = styled.div`
+  background-color: #F4EAE2;
+`;
+
+export const HMade = styled.p`
+  text-align: center;
+  font-size: 40px;
+  font-family: 'Recline';
+  padding: 20px;
+  color: #B69580;
+`;
+
+export const ThreeTestimonial = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100vh;
+  color: white;
+`;
+
+export const TestOne = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 33.3%;
+  padding: 30px 0;
+`;
+
+export const TestinImg = styled.img`
+  width: 60%;
+  margin: auto;
+  opacity: 0;
+`;
+
+export const Sec50 = styled.div`
+  width: 80%;
+`;
+
+export const TestHead = styled.p`
+  font-size: 20px;
+  font-family: 'Recline';
+  margin-top: 10px;
+`;
+
+export const TestContent = styled.p`
+  text-align: left;
+`;
